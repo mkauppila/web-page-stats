392eea95aaf752ffcdda5a4f31aca105950bdf11
Markus Kauppila <markus.kauppila@gmail.com>
2024-11-27 21:33:55 +0200

    Restructure the project

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
-generate-oas:
-	go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest 
+generate:
+	go generate -n -x ./...
+		
+
 
---
+++ internal/api/api-gen.go
@@ -0,0 +1,3 @@
+package api
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config ../../oas-config.yml ../../api-spec.yml
