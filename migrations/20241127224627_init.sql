407ed31529c1b514f85ffa6a9906c10b4c59a012
Markus Kauppila <markus.kauppila@gmail.com>
2024-12-01 11:03:50 +0200

    Update the base migration

--- migrations/20241127224627_init.sql
+++ migrations/20241127224627_init.sql
@@ -1,15 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE count (
-  category text NOT NULL,
+CREATE TABLE view_count (
+  category text CHECK(category IN ('articles','snippets')) NOT NULL,
   slug text NOT NULL,
-  type text CHECK(type IN ('articles','snippets')) NOT NULL,
-  count integer CHECK(count >0) NOT NULL,
+  count int CHECK(count > 0) NOT NULL,
+  PRIMARY KEY(category, slug)
+);
+
+CREATE TABLE reaction_count (
+  category text CHECK(category IN ('articles','snippets')) NOT NULL,
+  slug text NOT NULL,
+  love integer CHECK(love > 0) NOT NULL,
+  like integer CHECK(like > 0) NOT NULL,
+  mindblown integer CHECK(mindblown > 0) NOT NULL,
+  puzzling integer CHECK(puzzling > 0) NOT NULL,
   PRIMARY KEY(category, slug)
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE count;
+DROP TABLE view_count;
+DROP TABLE reaction_count;
 -- +goose StatementEnd
