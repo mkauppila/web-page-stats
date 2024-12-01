25a7616903a116c6c4890f1cc48a52e06f42f3f5
Markus Kauppila <markus.kauppila@gmail.com>
2024-12-01 12:04:09 +0200

    Add support for GETting view counts

--- internal/counters/view.go
+++ internal/counters/view.go
@@ -18,23 +18,37 @@ func CreateViewCounter(db *sql.DB) *ViewCounter {
 }
 
 func (v *ViewCounter) GetCount(category, slug string) (handler.ViewCount, error) {
-	return handler.ViewCount{}, nil
+	sql := `
+		SELECT count 
+  		  FROM view_count 
+         WHERE category = ?
+	       AND slug = ?`
+	results, err := v.db.QueryContext(context.Background(), sql, category, slug)
+	if err != nil {
+		return handler.ViewCount{}, err
+	}
+	defer results.Close()
+
+	var counts int
+	for results.Next() {
+		results.Scan(&counts)
+	}
+
+	return handler.ViewCount{
+		Count: counts,
+	}, nil
 }
 
 func (v *ViewCounter) Update(category, slug string) (handler.ViewCount, error) {
-	// INSERT INTO Book (ID, Name)
-	//  VALUES (1001, 'SQLite')
-	//  ON CONFLICT (ID) DO
-	//  UPDATE SET Name=excluded.Name;
 	sql := `
 		INSERT INTO view_count 
-			 VALUES (?, ?, 0)
+			 VALUES (?, ?, 1)
 		ON CONFLICT (category, slug) DO
   	         UPDATE SET count=count + 1
 	  	  RETURNING count`
 	results, err := v.db.QueryContext(context.Background(), sql, category, slug)
 	if err != nil {
-		panic(err)
+		return handler.ViewCount{}, err
 	}
 	defer results.Close()
 
